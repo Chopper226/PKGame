ccc08b09838cc62c144606fe0f52e4e7fe4cd8a0
leeeee <leeeee940226@gmail.com>
2024-05-19 21:03:37 +0800

    init frame

---
+++ src/Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    Panel panel;
+
+    Frame(){
+        panel = new Panel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
