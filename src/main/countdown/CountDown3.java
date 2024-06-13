ea2c2d4e5e1ae08537b3b253211688cf0c73146c
LAPTOP-61SEV0BJ\User <ditto02192200@gmail.com>
2024-06-13 18:23:07 +0800

    add countdown 3 image

--- src/main/countdown/CountDown3.java
+++ src/main/countdown/CountDown3.java
@@ -7,16 +7,27 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import javax.swing.ImageIcon;
+import java.awt.Image;
 
 public class CountDown3 extends Panel{
 
     private static Font font = new Font("Arial" , Font.BOLD , 100);
+    private Image backgroundImage;
 
+    public CountDown3() {
+        this.backgroundImage = new ImageIcon("res/interface/3.png").getImage(); 
+   
+    }
     public void paintComponent(Graphics g2) {
         super.paintComponent(g2);
 
         Graphics2D g = ((Graphics2D) g2); 
 
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+        
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
